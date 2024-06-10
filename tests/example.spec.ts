6a95e1de7346175f93c04cf027c41d5f188fd5e0
dz1dz1on <k.stepniak@vasco-electronics.com>
2024-06-10 18:48:02 +0200

    Add vcode settings and extensions

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,22 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test("has title", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
+  const array = page.locator(".col.col--6");
 
+  for (let i = 0; i < (await array.count()); i++) {
+    await array.nth(i).click();
+  }
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test("get started link", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
 
   // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  await page.getByRole("link", { name: "Get started" }).click();
 
   // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await expect(page.getByRole("heading", { name: "Installation" })).toBeVisible();
 });
